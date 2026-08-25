777898ebb71a25063ed0e07dc8a7d3e44ff51bba
Sven Primus <svenprimus@gmail.com>
2026-08-25 15:53:41 +0200

    moved statusbar to object - boss status working

--- models/game-objects/status-bar.class.js
+++ models/game-objects/status-bar.class.js
@@ -2,25 +2,33 @@ import { DrawableObject } from '../world/drawable-object.class.js';
 import { ImageLib } from '../utility/image-lib.class.js';
 
 export class StatusBar extends DrawableObject {
-    constructor(hCanvas, percentage) {
-        super(hCanvas).loadImage(ImageLib.STATUSBAR.hp.imgs[5]);
-        this.x = hCanvas * 0.05;
-        this.y = this.x;
+    isBoss = false;
+    constructor(wCanvas, hCanvas, movableObject, y, isBoss) {
+        super(hCanvas).loadImage(isBoss ? ImageLib.STATUSBAR.boss.imgs[5] : ImageLib.STATUSBAR.hp.imgs[5]);
+        this.y = y;
         this.w = hCanvas / 3;
-        this.h = ImageLib.STATUSBAR.hp.hNatural / (ImageLib.HERO.wNatural / this.w);
+        this.x = isBoss ? wCanvas - y - this.w : y;
+        this.h = ImageLib.STATUSBAR.hp.hNatural / (ImageLib.STATUSBAR.hp.wNatural / this.w);
+        this.isBoss = isBoss;
+        this.reverseDirection = isBoss;
         this.loadImagesToCache();
-        this.setPercentage(percentage);
+        this.setPercentage((100 * movableObject.hp) / movableObject.hpMax);
     }
 
     loadImagesToCache() {
         this.loadImages(ImageLib.STATUSBAR.hp.imgs);
+        this.loadImages(ImageLib.STATUSBAR.boss.imgs);
     }
 
     setPercentage(percentage) {
         const index = Math.min(
-            Math.round(((ImageLib.STATUSBAR.hp.imgs.length - 1) * percentage) / 100),
+            Math.ceil(
+                (((this.isBoss ? ImageLib.STATUSBAR.hp.imgs.length : ImageLib.STATUSBAR.hp.imgs.length) - 1) *
+                    percentage) /
+                    100
+            ),
             ImageLib.STATUSBAR.hp.imgs.length - 1
         );
-        this.playSingleImage(ImageLib.STATUSBAR.hp.imgs, index);
+        this.playSingleImage(this.isBoss ? ImageLib.STATUSBAR.boss.imgs : ImageLib.STATUSBAR.hp.imgs, index);
     }
 }
